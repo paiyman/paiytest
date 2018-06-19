c38f213c03875c7e70e5112d099f1ace2cdcd797
DESKTOP-UTBB7J6\paiyman <paiymanft@gmail.com>
2018-06-19 10:49:10 +0700

    no message

---
+++ Paiycourse/Paiy/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'this is simply', 'https://www.pcgamesn.com/sites/default/files/every%20sona%20skin.jpg'),
+    new Recipe('A test Recipe', 'this is simply', 'https://www.pcgamesn.com/sites/default/files/every%20sona%20skin.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
+}
